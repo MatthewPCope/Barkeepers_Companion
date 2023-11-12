4f51e7dc8b02c67a18b582da4f1ef7d593044b0e
DCAPNETTO <dan.caponetto@gmail.com>
2023-11-11 16:37:28 -0800

    made some changes to the Create Form

--- client/src/components/CocktailForm.js
+++ client/src/components/CocktailForm.js
@@ -1,18 +1,45 @@
-import React, {useState} from 'react'
-import {useNavigate, Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const CocktailForm = (props) => {
-    const {initialName, initialIngredients, initialTechnique, onSubmitProp} = props;
-    const [ name , setName ] = useState(initialName);
-    const [ ingredients , setIngredients ] = useState(initialIngredients);
-    const [ technique , setTechnique ] = useState(initialTechnique);
+    const { initialName, initialIngredients, initialTechnique, onSubmitProp } = props;
+    const [name, setName] = useState(initialName);
+    const [ingredients, setIngredients] = useState(initialIngredients);
+    const [technique, setTechnique] = useState(initialTechnique);
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate();
+
+    const isFormValid = () => {
+        const newErrors = {};
+
+        if (!name.trim()) {
+            newErrors.name = 'Name is required';
+        }
+
+        if (!ingredients.trim()) {
+            newErrors.ingredients = 'Ingredients are required';
+        }
+
+        if (!technique.trim()) {
+            newErrors.technique = 'Technique is required';
+        }
+
+        setErrors(newErrors);
+
+        // Form is valid if there are no errors
+        return Object.keys(newErrors).length === 0;
+    };
 
     const createCocktail = async (e) => {
         e.preventDefault();
+
+        if (!isFormValid()) {
+            console.log('Form is not valid. Please check your inputs.');
+            return;
+        }
+
         try {
             const submissionResult = await onSubmitProp({ name, ingredients, technique });
-            // Add Validations here as well in order to fix my redirect problem
             if (submissionResult) {
                 navigate('/cocktails');
             } else {
@@ -24,57 +51,59 @@ const CocktailForm = (props) => {
     };
 
     return (
-        <> 
-        
-            {/* <form onSubmit={createCocktail}successcallback={() => navigate('/cocktails')} > */}
+        <>
             <div className=''>
-                <h1 className=' font3 text-center mb-3 mt-5'>Create a Cocktail</h1>
+                <h1 className='font3 text-center mb-3 mt-5'>Create a Cocktail</h1>
                 <div id="container2">
-                    <div  className= ' font1 box p-4 my-3'>
-                        <form onSubmit={createCocktail} successcallback={() => navigate('/cocktails')} >
+                    <div className='font1 box p-4 my-3'>
+                        <form onSubmit={createCocktail}>
                             <div className="form-group mb-4">
                                 <label className='form-label'>Cocktail Name:</label>
-                                <input className='form-control'
+                                <input
+                                    className='form-control'
                                     onChange={(e) => setName(e.target.value)}
                                     value={name}
                                     name="name"
                                     type="text"
                                 />
+                                {errors.name && <div className="error">{errors.name}</div>}
                             </div>
-                            
+
                             <div className="form-group mb-4">
                                 <label className='form-label'>Ingredients</label>
-                                <textarea className='form-control'
+                                <textarea
+                                    className='form-control'
                                     onChange={(e) => setIngredients(e.target.value)}
                                     value={ingredients}
                                     name="ingredients"
                                     type="text"
                                     rows="4" cols="50"
                                 />
+                                {errors.ingredients && <div className="error">{errors.ingredients}</div>}
                             </div>
-                            
+
                             <div className="form-group mb-4">
                                 <label className='form-label'>Technique</label>
-                                <textarea className='form-control'
+                                <textarea
+                                    className='form-control'
                                     onChange={(e) => setTechnique(e.target.value)}
                                     value={technique}
                                     name="technique"
                                     type="text"
                                     rows="4" cols="50"
                                 />
+                                {errors.technique && <div className="error">{errors.technique}</div>}
                             </div>
                             <div className='text-center'>
-                            <input className="submit-input" type="submit" value="Submit" />
+                                <input className="submit-input" type="submit" value="Submit" />
                             </div>
-                            
+
                         </form>
-                        {/* <div className='text-center mt-3'> */}              
                     </div>
                 </div>
             </div>
-    </>
-           
+        </>
     );
-}
+};
 
 export default CocktailForm;
